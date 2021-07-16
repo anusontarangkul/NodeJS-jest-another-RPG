3890d42f35e0a8e7b9067538b85f794416e9f518
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-07-16 00:27:35 -0700

    reduceHealth method

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -46,3 +46,14 @@ test('checks if player is alive or not', () => {
     player.health = 0;
     expect(player.isAlive()).toBeFalsy();
 })
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
+})
